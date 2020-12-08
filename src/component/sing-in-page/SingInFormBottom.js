68f0007ee70b8016103e768a978376526c1c5562
teerawat <teerawat.kruelit@gmail.com>
2020-12-09 04:01:28 +0700

    update

--- src/component/sing-in-page/SingInFormBottom.js
+++ src/component/sing-in-page/SingInFormBottom.js
@@ -3,6 +3,21 @@ import Styled from 'styled-components'
 
 import { Hr, Flex } from '../../dist/styled/Master'
 
+const RegisterGroup = Styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 5px 0px 0px 0px;
+    
+    & > :not(first-child){
+        margin: 0px 0px 0px 5px;
+    }
+`;
+
+const Span = Styled.span`
+    font-size: 18px;
+`;
+
 const BtnFacebook = Styled.div`
     width: 90%;
     height: 50px;
@@ -16,9 +31,9 @@ export default function SingInBottom() {
     return (
         <div className="sing-in-form-bottom">
             <Hr color="#EC1C29"></Hr>
-            <Flex className="sing-in-form-bottom__register-group" cMargin="0px 0px 0px 10px">
-                ยังไม่ได้เป็นสมาชิก ? <a className="sing-in-form-bottom__link-register" href="/">สมัครสมาชิกใหม่</a>
-            </Flex>
+            <RegisterGroup className="sing-in-form-bottom__register-group">
+                <Span>ยังไม่ได้เป็นสมาชิก?</Span><a className="sing-in-form-bottom__link-register" href="/">สมัครสมาชิกใหม่</a>
+            </RegisterGroup>
             <Flex className="sing-in-form-bottom__btn-facebook-group">
                 <BtnFacebook className="sing-in-form-bottom__btn-facebook"></BtnFacebook>
             </Flex>
